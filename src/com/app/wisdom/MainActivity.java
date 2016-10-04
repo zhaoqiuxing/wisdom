9acd6d0c6f7f1ef436cf3f4f69aaf3004ea17e67
zqx <zqx@zqx.com>
2016-10-04 17:21:46 +0800

    fff

--- src/com/app/wisdom/MainActivity.java
+++ src/com/app/wisdom/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main); 
+		
 	}
 
 	@Override
