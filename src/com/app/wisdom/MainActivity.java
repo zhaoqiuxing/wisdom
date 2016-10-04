30727e3e677a5a56ebc4ffa8939a9d1e1aaa1039
zqx <zqx@zqx.com>
2016-10-04 16:16:42 +0800

    d

--- src/com/app/wisdom/MainActivity.java
+++ src/com/app/wisdom/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		System.out.println("");
 		setContentView(R.layout.activity_main);
 		
 		System.out.println();System.out.println();System.out.println();System.out.println();
